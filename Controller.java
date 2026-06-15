2d55192e3545c675a2ad923c445df3b4004dda6a
Mom Phanith <remanphanith@gmail.com>
2026-06-15 17:05:19 +0700

    d

--- Controller.java
+++ Controller.java
@@ -13,6 +13,7 @@ public class Controller {
 
     HBox hbox_1 = new HBox(10);
     HBox hbox_2 = new HBox(10);
+    HBox hbox_3 = new HBox(10);
 
     @FXML
     private ScrollPane scrollPane;
@@ -25,25 +26,32 @@ public class Controller {
     Label label_item_2_price = new Label("$10");
     ImageView imageview_item_2;
 
+    Label label_item_3       = new Label("Knife");
+    Label label_item_3_price = new Label("$15");
+    ImageView imageview_item_3;
+
     Spinner<Integer> spinner_item_1 = new Spinner<>(0, 10, 0);
     Spinner<Integer> spinner_item_2 = new Spinner<>(0, 10, 0);
+    Spinner<Integer> spinner_item_3 = new Spinner<>(0, 10, 0);
 
     @FXML
     void initialize() {
         // --- Load images safely (both items) ---
         imageview_item_1 = loadImage("Asset/bowl.jpg");
         imageview_item_2 = loadImage("Asset/cutting board.jpg");
+        imageview_item_3 = loadImage("Asset/KNIFT.jpg");
 
         // --- Size images ---
         setImageSize(imageview_item_1, 95, 88);
         setImageSize(imageview_item_2, 95, 88);
+        setImageSize(imageview_item_3, 95, 88);
 
         // --- Build rows ---
         hbox_1.getChildren().addAll(imageview_item_1, label_item_1, label_item_1_price, spinner_item_1);
         hbox_2.getChildren().addAll(imageview_item_2, label_item_2, label_item_2_price, spinner_item_2);
-
+        hbox_3.getChildren().addAll(imageview_item_3, label_item_3, label_item_3_price, spinner_item_3);
         // --- Add rows to VBox, set as ScrollPane content ---
-        vbox.getChildren().addAll(hbox_1, hbox_2);
+        vbox.getChildren().addAll(hbox_1, hbox_2, hbox_3);
         scrollPane.setContent(vbox);
     }
 
