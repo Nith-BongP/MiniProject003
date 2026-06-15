6fe32ec33a9d0903e5f35fb63629b63d339c611b
BCSotheany <bchansotheany@gmail.com>
2026-06-15 15:58:17 +0700

    1

--- Controller.java
+++ Controller.java
@@ -1,3 +1,17 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+
 public class Controller {
-    
+
+    @FXML
+    private Button button_submit;
+
+    @FXML
+    private ScrollPane scollPane;
+
+    @FXML
+    private Label total;
+
 }
